63831b13aa2cf271472ee2be4f6df512eee28d83
Nuhaseereen <seereen1703@gmail.com>
2024-10-06 15:15:07 +0530

    Create vote.c

---
+++ vote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int age;
+   scanf("%d",&age);
+   if(age>17)
+{
+    printf("you are eligible to vote");
+}
+else
+{
+    printf("not eligibe to vote");
+}
+    return 0;
+}
