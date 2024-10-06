cb917801ac7e9771d17040f56627591648883408
Nuhaseereen <seereen1703@gmail.com>
2024-10-06 16:15:28 +0530

    Update large3.c

--- large3.c
+++ large3.c
@@ -1,32 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {  //to print the largest number
 int n1,n2,n3;
 scanf("%d%d%d",&n1,&n2,&n3);
-if(n1>n2 && n1>n3)
+if(n1>=n2 && n1>=n3)
 {
     printf("%d",n1);
 }
-else if(n2>n3 && n2>n1)
-{
-    printf(" %d",n2);
-}
-else if(n3>n2 && n3>n1)
-{
-    printf(" %d",n3);
-}
-else if(n1==n2==n3)
-{
-    printf("%d",n3);
-}
-else if(n1==n2 || n1==n3)
-{
-    printf("%d",n1);
-}
-else if (n2==n3)
+else if(n2>=n3 && n2>=n1)
 {
     printf("%d",n2);
 }
-
+else
+{ printf("%d",n3);
+}
     return 0;
 } 
