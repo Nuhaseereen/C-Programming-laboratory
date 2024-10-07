e88375407b29568b6801603864d959901fdc99c3
Nuhaseereen <seereen1703@gmail.com>
2024-10-07 19:14:09 +0530

    Create Exp1_level1.c

---
+++ Exp1_level1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+ { //A is lenght,B is width and C is area
+ int A,B,C;
+ scanf("%d%d",&A,&B); 
+ C=A*B; 
+printf("area of rectangle:%d",C);
+    return 0;
+}
