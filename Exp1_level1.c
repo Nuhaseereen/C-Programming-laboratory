fcbc61bb1ec0dadca4d4b7b55a2478bfb5c85159
Nuhaseereen <seereen1703@gmail.com>
2024-10-07 19:19:12 +0530

    Update Exp1_level1.c

--- Exp1_level1.c
+++ Exp1_level1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 int main() 
- { //A is lenght,B is width and C is area
+ { //A is lenght,B is width and C is area. A and B are Natural numbers
  int A,B,C;
  scanf("%d%d",&A,&B); 
  C=A*B; 
